0eb32d72b9f6adb45ae22497803041d54ba7fb1d
ramilmavlihanov <Ramil040201@gmail.com>
2025-01-12 04:34:00 +0300

    module_17_3 done

---
+++ app_17_3/backend/db_depends.py
@@ -0,0 +1,9 @@
+from .db import sessionLocal
+
+
+async def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app_17_3/schemas/UserSchemas.py
+++ app_17_3/schemas/UserSchemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+
 class CreateUser(BaseModel):
     username: str
     firstname: str
@@ -11,15 +12,3 @@ class UpdateUser(BaseModel):
     firstname: str
     lastname: str
     age: int
-
-
-class CreateTask(BaseModel):
-    title: str
-    content: str
-    priority: int
-
-
-class UpdateTask(BaseModel):
-    title: str
-    content: str
-    priority: int
